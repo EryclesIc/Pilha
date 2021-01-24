c0d3968b28483ab47884208e1cd7bccf8d72d632
Flavia Soares <flaviasoares@ic.ufal.br>
2021-01-23 23:20:57 -0300

    mudanças

--- menu.py
+++ menu.py
@@ -3,39 +3,33 @@ import numpy as np
 import random
 import pandas as pd
 from starbucks import Starbucks
+from graph import Graph
 
 dados_planilha = pd.read_csv("https://raw.githubusercontent.com/EryclesIc/Pilha/main/directory.csv", encoding="UTF-8")
 
 quant_linhas = len(dados_planilha)
 print("Quantidade de linhas do dataframe é {}.\n".format(quant_linhas))
 
-linha = dados_planilha.iloc[2]
-grande_bosta = Starbucks(linha)
+grafo = Graph()
 
-print(grande_bosta)
-
-
-
-
-
-# grafo = grafoStarbucks()
-
-# def menu():
-#     quant_vertices = int(input("Digite a quantidade de vértices:\n"))
+def menu():
+    quant_vertices = int(input("Digite a quantidade de vértices:\n"))
     
-#     for i in range(quant_vertices):
-#         linha_aleatoria = random.randint(0, quant_linhas)-1
-#         linha = dados_planilha.iloc[linha_aleatoria]
-#         starbucks_linha = Starbucks(linha)
-        
-#         print(starbucks_linha)
-        #AQUI chame a função para adicionar as linhas no grafo
-        # grafo.add_vertex(linha, dados_planilha, i)
+    for i in range(quant_vertices):
+        indice_linha_aleatoria = random.randint(0, quant_linhas)-1
+        linha_dataframe = dados_planilha.iloc[indice_linha_aleatoria]
+        starbucks_linha = Starbucks(linha_dataframe)
+        # chave = starbucks_linha.store_number
+
+        # AQUI chame a função para adicionar as linhas no grafo
+        grafo.add_vertex(starbucks_linha)
+    
+    print(grafo.get_vertices())
         
     # AQUI chame a função para mostrar todos os vértices dentro do grafo
     # for j in range(quant_vertices):
     #     grafo.create_edge(j, quant_vertices, dados_planilha)
-    # # for k in range(quant_vertices): print(grafo.get_vertex(k))
+    # for k in range(quant_vertices): print(grafo.get_vertex(k))
     # grafo.get_vertices()
 
     
@@ -52,5 +46,5 @@ print(grande_bosta)
     # for v in grafo:
     #     print ('grafo.vert_dict[%s]=%s' %(v.get_id(), grafo.vert_dict[v.get_id()]))
     # AQUI chame a função que vai calcular a menor distância entre esses nós
-#     menu()
-# menu()
+    menu()
+menu()
--- starbucks.py
+++ starbucks.py
@@ -26,4 +26,3 @@ class Starbucks:
                                                                     self.timezone,
                                                                     self.longitude,
                                                                     self.latitude)
-
